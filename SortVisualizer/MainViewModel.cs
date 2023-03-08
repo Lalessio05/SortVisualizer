9d17f9974e996bff390ecd4845d6a70d5e9f92e1
catri <catrini.alessio2@gmail.com>
2023-03-08 12:22:27 +0100

    FUNGE FORSE

--- SortVisualizer/MainViewModel.cs
+++ SortVisualizer/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System;
 namespace SortVisualizer
 {
 
@@ -15,12 +16,15 @@ namespace SortVisualizer
         {
             // Inizializza la collezione di oggetti con i dati che devono essere visualizzati
             Items = new ObservableCollection<MyDataObject>();
-            for (int i = 0; i < 20; i++) { 
-                Items.Add(new MyDataObject((i+1)*10,"Black"));
+            int n = 380;
+            for (int i = 0; i < n; i++)
+            {
+                Items.Add(new MyDataObject(new Random().Next(10, 300), "Black", 380/ n));
             }
 
 
             SortCommand = new RelayCommand(Sort);
+ 
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -30,7 +34,7 @@ namespace SortVisualizer
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        
+
 
         private ObservableCollection<MyDataObject> _items;
         public ObservableCollection<MyDataObject> Items
@@ -50,21 +54,28 @@ namespace SortVisualizer
             for (int i = 1; (i <= (Items.Count - 1)) && flag; i++)
             {
                 flag = false;
-                Items[i].Colore = "Green";
                 for (int j = 0; j < (Items.Count - 1); j++)
                 {
                     count = count + 1;
+                    Items[j].Colore = "Green"; 
+                    Items[j + 1].Colore = "Red"; 
                     if (Items[j + 1].Altezza > Items[j].Altezza)
                     {
-                        await Task.Delay(50);
+                        //await Task.Delay(1);
                         var temp = Items[j];
                         Items[j] = Items[j + 1];
                         Items[j + 1] = temp;
                         flag = true;
-                        await Task.Delay(10);
                     }
+                    Items[j].Colore = "Black"; // entrambi gli elementi tornano neri dopo lo scambio
+                    Items[j + 1].Colore = "Black";
                 }
             }
+#if DEBUG
+            foreach (var item in Items) { item.Colore = "Green"; }
+#endif
+
+            Items[0].Colore = "Green"; // l'elemento più piccolo rimane verde
         }
 
 
@@ -73,14 +84,30 @@ namespace SortVisualizer
 
 
 
-        public class MyDataObject
+        public class MyDataObject : BaseNotification
         {
+            private string _colore;
             public int Altezza { get; set; }
-            public string Colore { get; set; }
-            public MyDataObject(int altezza, string colore) {
+            public string Colore
+            {
+                get { return _colore; }
+                set
+                {
+                    if (_colore != value)
+                    {
+                        _colore = value;
+                        OnPropertyChanged(nameof(Colore));
+                    }
+                }
+            }
+            public int AltezzaElemento { get; set; }
+
+            public MyDataObject(int altezza, string colore, int altezzaElemento)
+            {
                 Altezza = altezza;
                 Colore = colore;
-            }   
+                AltezzaElemento = altezzaElemento;
+            }
         }
     }
 }
